a33a33aa45e33c576c6f6e59d4363f2bead2fceb
Joe <jsbenning4@gmail.com>
2017-03-25 21:22:01 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
+
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+      
+
+//       var assignee = $('#repoOwner').val();
+//       var repo = $('#repoName').val();
+//       var title = $('#title').val();
+//       var body = $('#body').val();
+//       console.log(typeof(GithubInteractor.testMe()));//(repo);
+//       event.preventDefault();
+//   });
+
+// });
+
 class GithubInteractor {
   constructor(token) {
     this.token = token;
   }
 
-  // $("form").on("submit", function(event) {
-  //   event.preventDefault();
 
-  //   var assignee = $('#repoOwner').val();
-  //   var repo = $('#repoName').val();
-  //   var title = $('#title').val();
-  //   var body = $('#body').val();
-  
-  //   createIssue(repo, assignee, title, body);
-  // });
-
-
-
-
-  createIssue(repo, assignee, title, body ) {
-    var url = "/repos/" + assignee + "/" + repo + "/" + "issues";
-    var myData = {title: title, body: body};
+//   createIssue(repo, assignee, title, body ) {
+//     var url = "/repos/" + assignee + "/" + repo + "/" + "issues";
+//     var myData = {title: title, body: body};
     
 
-    $.ajax({
-      type: "POST",
-      data: JSON.stringify(myData),
-      url: url,
-      contentType: "application/json"
-    })
+//     $.ajax({
+//       type: "POST",
+//       data: JSON.stringify(myData),
+//       url: url,
+//       contentType: "application/json"
+//     })
 
-    .done(handleResponse(response));
-    .fail(handleError(xhr, textStatus, errorThrown);
-  }
+//     .done(handleResponse(response))
+//     .fail(handleError(xhr, textStatus, errorThrown));
+//   }
 
   // handleResponse: function(response){
   //   console.log('response', response);
@@ -41,7 +43,24 @@ class GithubInteractor {
   //   console.log(xhr.responseText);
   // }
 
-  
+  testMe(repo) {
+    console.log(repo);
+    event.preventDefault();
+  }  
 } 
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+      var testy = new GithubInteractor("43344");
+
+      var assignee = $('#repoOwner').val();
+      var repo = $('#repoName').val();
+      var title = $('#title').val();
+      var body = $('#body').val();
+      console.log(typeof(testy));//(repo);
+      event.preventDefault();
+  });
+
+});
 
 
