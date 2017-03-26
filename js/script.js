3bd92657480cca185274b741420c9946c51d618e
Joe <jsbenning4@gmail.com>
2017-03-25 22:09:26 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,64 +1,53 @@
-
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-      
-
-//       var assignee = $('#repoOwner').val();
-//       var repo = $('#repoName').val();
-//       var title = $('#title').val();
-//       var body = $('#body').val();
-//       console.log(typeof(GithubInteractor.testMe()));//(repo);
-//       event.preventDefault();
-//   });
-
-// });
-
 class GithubInteractor {
   constructor(token) {
     this.token = token;
   }
+}
 
+function createIssue(repo, assignee, title, body ) {
+  var url = "https://api.github.com/repos/" + assignee + "/" + repo + "/" + "issues";
+  var myData = {title: title, body: body};
+  //console.log(myData);
+  
 
-//   createIssue(repo, assignee, title, body ) {
-//     var url = "/repos/" + assignee + "/" + repo + "/" + "issues";
-//     var myData = {title: title, body: body};
-    
+  $.ajax({
+    type: "POST",
+    data: JSON.stringify(myData),
+    url: url,
+    contentType: "application/json"
+  })
 
-//     $.ajax({
-//       type: "POST",
-//       data: JSON.stringify(myData),
-//       url: url,
-//       contentType: "application/json"
-//     })
+  //.done(handleResponse(response))
+  .done(function(res) {
+    handleResponse(res);
+  })
+  //.fail(handleError(xhr, textStatus, errorThrown));
+  .fail(function(xhr, textStatus, errorThrown){
+    handleError(xhr, textStatus, errorThrown);
+  })
+}
 
-//     .done(handleResponse(response))
-//     .fail(handleError(xhr, textStatus, errorThrown));
-//   }
+function handleResponse(response){
+  console.log('response', response);
+}
 
-  // handleResponse: function(response){
-  //   console.log('response', response);
-  // }
+function handleError(xhr, status, error) {
+  console.log(xhr.responseText);
+}
+ 
 
-  // handleError: function(xhr, status, error) {
-  //   console.log(xhr.responseText);
-  // }
-
-  testMe(repo) {
-    console.log(repo);
-    event.preventDefault();
-  }  
-} 
 
 $(document).ready(function() {
   $("form").submit(function(event) {
-      var testy = new GithubInteractor("43344");
+    event.preventDefault();
 
-      var assignee = $('#repoOwner').val();
-      var repo = $('#repoName').val();
-      var title = $('#title').val();
-      var body = $('#body').val();
-      console.log(typeof(testy));//(repo);
-      event.preventDefault();
+    var assignee = $('#repoOwner').val();
+    var repo = $('#repoName').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+
+    createIssue(repo, assignee, title, body);
+    
   });
 
 });
